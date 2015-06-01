a50c7c411bc44795ee93ae8d8f1be5627bd32a04
micahstairs <micah.stairs@gmail.com>
2015-06-01 10:46:51 -0300

    Javadoc

--- Event.java
+++ Event.java
@@ -1,99 +1,137 @@
+/**
+ * Event -  This simple class represents an event in an automaton. It supports both centralized and decentralized control, which
+ *			means that it can have observability and controllability properties for each controller. It also has support for
+ *			events that have labels formatted as vectors. 
+ *
+ * @author Micah Stairs
+ *
+ * TABLE OF CONTENTS:
+ *	-Class Constants
+ *	-Instance Variables
+ *	-Constructors
+ *	-Automata Operations
+ *	-Image Generation
+ *	-GUI Input Code Generation
+ *	-Working with Files
+ *	-Miscellaneous
+ *	-Mutator Methods
+ *	-Accessor Methods
+ **/
+
 import java.util.*;
 
 public class Event implements Comparable<Event> {
     
-    	/* Class constants */
+    	/** CLASS CONSTANTS **/
 
-    // 255 unique events with 0 representing null
-	// NOTE: We will make this flexible later to allow for more events
+    // Current ID system allows for 255 unique events with 0 representing null (NOTE: We will make this flexible later to allow for more events)
     public static final int MAX_NUMBER_OF_EVENTS = 255;
 	public static final int N_BYTES_OF_ID = 1; 
 
-    	/* Private instance variables */
+    	/** PRIVATE INSTANCE VARIABLES **/
 
     private String label;
     private int id;
     private boolean[] observable, controllable;
 
+    // Events can sometimes be vectors (for example, automata created by synchonrized composition use them) 
+    // Ex: "<a_b_d>" actually represents an edge vector: {"a", "b", "d"}
+    private String[] labelVector = null;
+
+    	/** CONSTRUCTOR **/
+
     /**
-     * 	Construct a new event with the specified properties.
-     *	@param label - The name of the state
-     *	@param observable - Whether or not the event can be observed
-     *	@param controllable - Whether or not the event can be controlled
+     * Construct a new event with the specified properties.
+     * @param label			The name of the state
+     * @param observable	Whether or not the event can be observed
+     * @param controllable	Whether or not the event can be controlled
      **/
 	public Event(String label, int id, boolean[] observable, boolean[] controllable) {
+
 		this.label = label;
 		this.id = id;
 		this.observable = observable;
 		this.controllable = controllable;
+
+		// Turn the label into a vector (if applicable)
+		createLabelVector();
+
 	}
+		/** VECTOR PARSING **/
 
-	// This is necessary when every element of the set needs to be checked (and we can't rely on using .contains because of how it handles HashSets and TreeSets).
-	// This method is only needed to be used in cases where events are coming from different automata (so the IDs are all out of whack)
-	public static boolean isElementOfSet(Set<Event> set, Event event) {
+	/**
+	 * If the event label is formatted as a vector, break it up into its components.
+	 **/
+	private void createLabelVector() {
 
-		for (Event e : set)
-			if (e.equals(event))
-				return true;
-
-		return false;
+		// Ensure that the label is a vector
+		if (label.charAt(0) == '<' && label.charAt(label.length() - 1) == '>')
+			labelVector = label.substring(1, label.length() - 1).split("_");
 
 	}
 
 		/** STANDARD ACCESSOR AND MUTATOR METHODS **/
 
 	/**
-	 *	Get the label of the event
- 	 *	@return label
+	 * Get the label of the event
+ 	 * @return label
 	 **/
 	public String getLabel() {
 		return label;
 	}
 
 	/**
-	 *	Get the ID number of the event
- 	 *	@return id
+	 * Get the ID number of the event
+ 	 * @return id
 	 **/
 	public int getID() {
 		return id;
 	}
 
 	/**
-	 *	Get the observability property of the event for each controller.
- 	 *	@return whether or not the event is observable
+	 * Get the observability property of the event for each controller.
+ 	 * @return whether or not the event is observable
 	 **/
 	public boolean[] isObservable() {
 		return observable;
 	}
 
 	/**
-	 *	Get the controllability property of the event for each controller.
- 	 *	@return whether or not the event is controllable
+	 * Get the controllability property of the event for each controller.
+ 	 * @return whether or not the event is controllable
 	 **/
 	public boolean[] isControllable() {
 		return controllable;
 	}
 
+		public String getLabelFromVector(int index) {
+
+		if (labelVector == null)
+			return null;
+		else
+			return labelVector[index];
+		
+	}
+
 		/** OVERRIDDEN METHODS **/
 
+	/**
+	 * Returns a hash code for this event, based on the hash code of it's label (which is a string).
+	 * @return a hash code value for this object
+	 **/
 	@Override public int hashCode() {
 		return label.hashCode();
 	}
 
+	/**
+	 * Returns a value from the set {-1, 0, 1} to represent the comparison of two events (based on ID, which is an integer).
+	 * @param other	The other event that we're comparing this one to
+	 * @return -1 if this event comes before, 0 if the events are equal, 1 if this event comes after
+	 **/
 	@Override public int compareTo(Event other) {
-
-		// if (this.label.equals(other.label))
-		// 	return 0;
 
 		return (new Integer(id)).compareTo(other.getID());
 
-		// int comparedValue = (new Integer(id)).compareTo(other.getID());
-
-		// // This check is necessary in case events are coming from different automata (so they may have the same ID but have different labels)
-		// if (comparedValue == 0)
-		// 	return 1;
-
-		// return comparedValue;
 	}
 
 	/**
@@ -110,6 +148,10 @@ public class Event implements Comparable<Event> {
 
 	}
 
+	/**
+	 * Turn this event into a more meaningful representation as a string.
+	 * @return string representation
+	 **/
 	@Override public String toString() {
 
 		return "("
