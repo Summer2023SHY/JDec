0ea4301c8f0a0fcd0106be0f8b947b3159894087
micahstairs <micah.stairs@gmail.com>
2015-09-05 18:32:12 -0300

    files for second example

--- src/Liu.java
+++ src/Liu.java
@@ -7,23 +7,32 @@ public class Liu {
 
   public static void main(String[] args) throws IncompatibleAutomataException {
     perms();
-    // first();
-    // second();
+    // permsFirstAutomaton();
   }
 
 	public static void perms() {
 
     // Plants
     List<Automaton> plants = new ArrayList<Automaton>();
-    plants.add(new Automaton(new File("Thesis/SENDER.hdr"), new File("Thesis/SENDER.bdy"), false));
-    plants.add(new Automaton(new File("Thesis/RECEIVER.hdr"), new File("Thesis/RECEIVER.bdy"), false));
-    plants.add(new Automaton(new File("Thesis/CHANNEL.hdr"), new File("Thesis/CHANNEL.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/SenderB.hdr"), new File("Thesis/SecondExample/SenderB.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ReceiverB.hdr"), new File("Thesis/SecondExample/ReceiverB.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ChannelRS.hdr"), new File("Thesis/SecondExample/ChannelRS.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ChannelSR.hdr"), new File("Thesis/SecondExample/ChannelSR.bdy"), false));
 
     // Specifications
     List<Automaton> specs = new ArrayList<Automaton>();
-    specs.add(new Automaton(new File("Thesis/SpecSNDR.hdr"), new File("Thesis/SpecSNDR.bdy"), false));
-    specs.add(new Automaton(new File("Thesis/SpecRCVR.hdr"), new File("Thesis/SpecRCVR.bdy"), false));
-    specs.add(new Automaton(new File("Thesis/SpecSEQ.hdr"), new File("Thesis/SpecSEQ.bdy"), false));
+
+    // // Plants
+    // List<Automaton> plants = new ArrayList<Automaton>();
+    // plants.add(new Automaton(new File("Thesis/SENDER.hdr"), new File("Thesis/SENDER.bdy"), false));
+    // plants.add(new Automaton(new File("Thesis/RECEIVER.hdr"), new File("Thesis/RECEIVER.bdy"), false));
+    // plants.add(new Automaton(new File("Thesis/CHANNEL.hdr"), new File("Thesis/CHANNEL.bdy"), false));
+
+    // // Specifications
+    // List<Automaton> specs = new ArrayList<Automaton>();
+    // specs.add(new Automaton(new File("Thesis/SpecSNDR.hdr"), new File("Thesis/SpecSNDR.bdy"), false));
+    // specs.add(new Automaton(new File("Thesis/SpecRCVR.hdr"), new File("Thesis/SpecRCVR.bdy"), false));
+    // specs.add(new Automaton(new File("Thesis/SpecSEQ.hdr"), new File("Thesis/SpecSEQ.bdy"), false));
 
     // G{Sigma*}
     Automaton gSigmaStar = new Automaton(new File("Thesis/G_SIGMA_STAR.hdr"), new File("Thesis/G_SIGMA_STAR.bdy"), false);
@@ -47,6 +56,7 @@ public class Liu {
                 boolean insertSpecAtStart = (j==1);
                 boolean smallestNumberOfStepsToCounterExample = (k==1);
                 System.out.println("\tChoose plant before specification: " + choosePlantFirst);
+                System.out.println("\tFirst automaton will be chosen");
                 System.out.println("\tInsert specification at start of list: " + insertSpecAtStart);
                 System.out.println("\tChoosing automata with smallest number of steps needed to reject counter-example: " + smallestNumberOfStepsToCounterExample);
                 System.out.println("\tResult: " + incrementalVerification(new ArrayList<Automaton>(plantPerm), new ArrayList<Automaton>(specPerm), gSigmaStar, choosePlantFirst, insertSpecAtStart, smallestNumberOfStepsToCounterExample));
@@ -254,57 +264,184 @@ public class Liu {
 
   }
 
-  // public static void first() throws IncompatibleAutomataException {
 
-  //   // Plants
-  //   List<Automaton> plants = new ArrayList<Automaton>();
-  //   plants.add(new Automaton(new File("aut/Thesis/RECEIVER.hdr"), new File("aut/Thesis/RECEIVER.bdy"), false));
-  //   plants.add(new Automaton(new File("aut/Thesis/SENDER.hdr"), new File("aut/Thesis/SENDER.bdy"), false));
-  //   plants.add(new Automaton(new File("aut/Thesis/CHANNEL.hdr"), new File("aut/Thesis/CHANNEL.bdy"), false));
+/**
 
-  //   // Specifications
-  //   List<Automaton> specs = new ArrayList<Automaton>();
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecRCVR.hdr"), new File("aut/Thesis/SpecRCVR.bdy"), false));
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecSEQ.hdr"), new File("aut/Thesis/SpecSEQ.bdy"), false));
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecSNDR.hdr"), new File("aut/Thesis/SpecSNDR.bdy"), false));
 
-  //   // G{Sigma*}
-  //   Automaton gSigmaStar = new Automaton(new File("aut/Thesis/G_SIGMA_STAR.hdr"), new File("aut/Thesis/G_SIGMA_STAR.bdy"), false);
 
-  //   System.out.println("\tOrder of plants: " + plants);
-  //   System.out.println("\tOrder of specifications: " + specs);
-  //   boolean choosePlantFirst = true;
-  //   System.out.println("\tChoose plant before specification: " + choosePlantFirst);
-  //   System.out.println("\tResult: " + incrementalVerification(new ArrayList<Automaton>(plants), new ArrayList<Automaton>(specs), gSigmaStar, choosePlantFirst));
-  //   System.out.println();
 
-  // }
 
-  // public static void second() throws IncompatibleAutomataException {
 
-  //   // Plants
-  //   List<Automaton> plants = new ArrayList<Automaton>();
-  //   plants.add(new Automaton(new File("aut/Thesis/RECEIVER.hdr"), new File("aut/Thesis/RECEIVER.bdy"), false));
-  //   plants.add(new Automaton(new File("aut/Thesis/SENDER.hdr"), new File("aut/Thesis/SENDER.bdy"), false));
-  //   plants.add(new Automaton(new File("aut/Thesis/CHANNEL.hdr"), new File("aut/Thesis/CHANNEL.bdy"), false));
+**/
 
-  //   // Specifications
-  //   List<Automaton> specs = new ArrayList<Automaton>();
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecSNDR.hdr"), new File("aut/Thesis/SpecSNDR.bdy"), false));
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecSEQ.hdr"), new File("aut/Thesis/SpecSEQ.bdy"), false));
-  //   specs.add(new Automaton(new File("aut/Thesis/SpecRCVR.hdr"), new File("aut/Thesis/SpecRCVR.bdy"), false));
 
-  //   // G{Sigma*}
-  //   Automaton gSigmaStar = new Automaton(new File("aut/Thesis/G_SIGMA_STAR.hdr"), new File("aut/Thesis/G_SIGMA_STAR.bdy"), false);
+    public static void permsFirstAutomaton() {
 
-  //   System.out.println("\tOrder of plants: " + plants);
-  //   System.out.println("\tOrder of specifications: " + specs);
-  //   boolean choosePlantFirst = true;
-  //   System.out.println("\tChoose plant before specification: " + choosePlantFirst);
-  //   System.out.println("\tResult: " + incrementalVerification(new ArrayList<Automaton>(plants), new ArrayList<Automaton>(specs), gSigmaStar, choosePlantFirst));
-  //   System.out.println();
+      // Plants
+    List<Automaton> plants = new ArrayList<Automaton>();
+    plants.add(new Automaton(new File("Thesis/SecondExample/SenderB.hdr"), new File("Thesis/SecondExample/SenderB.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ReceiverB.hdr"), new File("Thesis/SecondExample/ReceiverB.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ChannelRS.hdr"), new File("Thesis/SecondExample/ChannelRS.bdy"), false));
+    plants.add(new Automaton(new File("Thesis/SecondExample/ChannelSR.hdr"), new File("Thesis/SecondExample/ChannelSR.bdy"), false));
 
-  // }
+    // Specifications
+    List<Automaton> specs = new ArrayList<Automaton>();
+    specs.add(new Automaton(new File("Thesis/SecondExample/Specification.hdr"), new File("Thesis/SecondExample/Specification.bdy"), false));
+
+    // // Plants
+    // List<Automaton> plants = new ArrayList<Automaton>();
+    // plants.add(new Automaton(new File("Thesis/SENDER.hdr"), new File("Thesis/SENDER.bdy"), false));
+    // plants.add(new Automaton(new File("Thesis/RECEIVER.hdr"), new File("Thesis/RECEIVER.bdy"), false));
+    // plants.add(new Automaton(new File("Thesis/CHANNEL.hdr"), new File("Thesis/CHANNEL.bdy"), false));
+
+    // // Specifications
+    // List<Automaton> specs = new ArrayList<Automaton>();
+    // specs.add(new Automaton(new File("Thesis/SpecSNDR.hdr"), new File("Thesis/SpecSNDR.bdy"), false));
+    // specs.add(new Automaton(new File("Thesis/SpecRCVR.hdr"), new File("Thesis/SpecRCVR.bdy"), false));
+    // specs.add(new Automaton(new File("Thesis/SpecSEQ.hdr"), new File("Thesis/SpecSEQ.bdy"), false));
+
+    // G{Sigma*}
+    Automaton gSigmaStar = new Automaton(new File("Thesis/G_SIGMA_STAR.hdr"), new File("Thesis/G_SIGMA_STAR.bdy"), false);
+
+    try {
+      
+      // All perms
+      List<List<Automaton>> plantPerms = generatePerm(plants);
+      List<List<Automaton>> specPerms = generatePerm(specs);
+      int nWays = plantPerms.size()*specPerms.size()*2  ;
+      int counter = 0;
+      for (List<Automaton> plantPerm : plantPerms)
+        for (List<Automaton> specPerm : specPerms)
+          // for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++) {
+              // for (int k = 0; k < 2; k++) {
+                System.out.printf("Trial %d/%d:\n", ++counter, nWays);
+                System.out.println("\tOrder of plants: " + plantPerm);
+                System.out.println("\tOrder of specifications: " + specPerm);
+                // boolean choosePlantFirst = (i==1);
+                boolean insertSpecAtStart = (j==1);
+                // boolean smallestNumberOfStepsToCounterExample = (k==1);
+                // System.out.println("\tChoose plant before specification: " + choosePlantFirst);
+                System.out.println("\tFirst automaton will be chosen");
+                System.out.println("\tInsert specification at start of list: " + insertSpecAtStart);
+                // System.out.println("\tChoosing automata with smallest number of steps needed to reject counter-example: " + smallestNumberOfStepsToCounterExample);
+                System.out.println("\tResult: " + incrementalVerificationChooseFirstAutomaton(new ArrayList<Automaton>(plantPerm), new ArrayList<Automaton>(specPerm), gSigmaStar, /*choosePlantFirst,*/ insertSpecAtStart/*, smallestNumberOfStepsToCounterExample*/));
+                System.out.println();
+              }
+
+    } catch (IncompatibleAutomataException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  /**
+   * NOTE: All plants and specs must have the entire alphabet of events listed (even if they are inactive
+   *       in that particular automaton. This is not currently being checked for.
+   * NOTE: Duplicates are being made in temporary files so as to not modify the originals.
+   **/
+  public static boolean incrementalVerificationChooseFirstAutomaton(List<Automaton> plants, List<Automaton> specs, Automaton gSigmaStar, /*boolean pickShortestCounterExample, */boolean insertSpecAtStart/*, boolean smallestNumberOfStepsToCounterExample*/) throws IncompatibleAutomataException {
+
+    // Create duplicates in order to prevent the originals from being modified
+    plants = duplicateList(plants);
+    specs = duplicateList(specs);
+
+    // Add self-loops to all plants and specifications
+    addSelfLoops(plants);
+    addSelfLoops(specs);
+
+    boolean choosePlantFirst = false; // this is toggled before first used so we actually choose a plant first
+
+    int iterationOuter = 0;
+    int iterationInner = 0;
+    while (specs.size() > 0) {
+
+      iterationOuter++;
+      System.out.printf("\t\tStarting outer loop iteration #%d.\n", iterationOuter);
+
+      Automaton kPrime = specs.get(0);
+      Automaton lPrime = gSigmaStar.duplicate();
+
+      // Temporary
+      List<Automaton> automataInKPrime = new ArrayList<Automaton>();
+      automataInKPrime.add(kPrime);
+      List<Automaton> automataInLPrime = new ArrayList<Automaton>();
+      automataInLPrime.add(lPrime);
+
+      List<List<String>> counterExample;
+      loop: while ( (counterExample = hasCounterExample(lPrime, kPrime)) != null ) {
+        
+        iterationInner++;
+        System.out.printf("\t\t\tStarting inner loop iteration #%d.\n", iterationInner);
+
+        choosePlantFirst = !choosePlantFirst;
+
+        // Choose one
+        if (choosePlantFirst) {
+
+          for (Automaton automaton : plants) {
+            if (!automataInLPrime.contains(automaton) && automaton.acceptsCounterExample(counterExample) != -1) {
+              lPrime = Automaton.intersection(lPrime, automaton, null, null);
+              System.out.println("\t\t\t\tPicking automaton from L\\L': " + automaton);
+              automataInLPrime.add(automaton);
+              continue loop;
+            }
+          }
+
+          for (Automaton automaton : specs) {
+            if (!automataInKPrime.contains(automaton) && automaton.acceptsCounterExample(counterExample) != -1) {
+              kPrime = Automaton.intersection(kPrime, automaton, null, null);
+              System.out.println("\t\t\t\tPicking automaton from K\\K': " + automaton);
+              automataInKPrime.add(automaton);
+              continue loop;
+            }
+          }
+
+        } else {
+
+          for (Automaton automaton : specs) {
+            if (!automataInKPrime.contains(automaton) && automaton.acceptsCounterExample(counterExample) != -1) {
+              kPrime = Automaton.intersection(kPrime, automaton, null, null);
+              System.out.println("\t\t\t\tPicking automaton from K\\K': " + automaton);
+              automataInKPrime.add(automaton);
+              continue loop;
+            }
+          }
+
+          for (Automaton automaton : plants) {
+            if (!automataInLPrime.contains(automaton) && automaton.acceptsCounterExample(counterExample) != -1) {
+              lPrime = Automaton.intersection(lPrime, automaton, null, null);
+              System.out.println("\t\t\t\tPicking automaton from L\\L': " + automaton);
+              automataInLPrime.add(automaton);
+              continue loop;
+            }
+          }
+
+        }
+
+        System.out.printf("\tRequired %d outer iterations and a total of %d inner iterations.\n", iterationOuter, iterationInner);
+        return false;
+
+      }
+
+      for (Automaton a : automataInKPrime)
+        if (!plants.contains(a)) {
+          if (insertSpecAtStart)
+            plants.add(0, a);
+          else
+            plants.add(a);
+        }
+      specs.removeAll(automataInKPrime);
+
+      System.out.println("\t\t# States in L': " + lPrime.getNumberOfStates());
+      System.out.println("\t\t# Transitions in L': " + lPrime.getNumberOfTransitions());
+    
+    }
+    
+    System.out.printf("\tRequired %d outer iterations and a total of %d inner iterations.\n", iterationOuter, iterationInner);
+    return true;
+
+  }
 
 }
 
