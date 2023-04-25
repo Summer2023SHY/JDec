d44c9906b57c3b53cd9cfdfa264c40a24154d8e7
syoon2 <syoon2@mta.ca>
2023-04-25 10:42:16 +0900

    Javadoc for access file implementations

--- src/main/java/com/github/automaton/io/BodyAccessFile.java
+++ src/main/java/com/github/automaton/io/BodyAccessFile.java
@@ -4,12 +4,23 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
+/**
+ * I/O handler for {@code .bdy} files.
+ * 
+ * @author Sung Ho Yoon
+ */
 public final class BodyAccessFile implements Closeable {
     private String bodyFileName;
     private File bodyFile;
     /** List each state in the automaton, with the transitions */
     private RandomAccessFile bodyRAFile;
 
+    /**
+     * Constructs a new {@code BodyAccessFile} for a given file
+     * @param bodyFile a body file
+     * @throws NullPointerException if argument is {@code null}
+     * @throws FileNotFoundException if the given file object cannot be opened
+     */
     public BodyAccessFile(File bodyFile) throws FileNotFoundException {
         this.bodyFile = Objects.requireNonNull(bodyFile);
         this.bodyFileName   = this.bodyFile.getAbsolutePath();
@@ -20,6 +31,10 @@ public final class BodyAccessFile implements Closeable {
         return bodyFile.exists();
     }
 
+    /**
+     * Clears the content of the body file
+     * @return {@code true} the content is cleared; {@code false} otherwise.
+     */
     public boolean clearFile() {
         try {
             bodyRAFile.close();
@@ -33,10 +48,22 @@ public final class BodyAccessFile implements Closeable {
         }
     }
 
+    /**
+     * Copies the content of the underlying body file to a new file
+     * 
+     * @param newHeaderFile the target destination
+     * @throws IOException if I/O error occurs
+     */
     public void copyTo(File newBodyFile) throws IOException {
         Files.copy(bodyFile.toPath(), newBodyFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
+    /**
+     * Copies and overwrites the underlying body file with the specified file
+     * 
+     * @param newHeaderFile the source data file
+     * @throws IOException if I/O error occurs
+     */
     public void copyFrom(File newFile) throws IOException {
         Objects.requireNonNull(newFile);
         if (!newFile.isFile()) {
@@ -47,16 +74,43 @@ public final class BodyAccessFile implements Closeable {
         bodyRAFile = new RandomAccessFile(bodyFile, "rw");
     }
 
+    /**
+     * Returns the underlying {@link RandomAccessFile}
+     * @return the underlying {@link RandomAccessFile}
+     */
     RandomAccessFile getRAFile() {
+        /*
+         * FOR INTERNAL USE ONLY! THIS METHOD IS
+         * PACKAGE-PRIVATE FOR A REASON.
+         */
         return bodyRAFile;
     }
 
+    /**
+     * Returns the underlying file object
+     * @return the underlying file object
+     */
     public File getBodyFile() {
         return bodyFile;
     }
 
+    /**
+     * Closes this body access file and releases any system resource
+     * associated with this.
+     * 
+     * @throws IOException if I/O error occurs
+     */
     @Override
     public void close() throws IOException {
         bodyRAFile.close();
+    }
+
+    /**
+     * Returns the string representation of this body access file
+     * @return the string representation of this body access file
+     */
+    @Override
+    public String toString() {
+        return this.bodyFileName;
     }
 }
--- src/main/java/com/github/automaton/io/HeaderAccessFile.java
+++ src/main/java/com/github/automaton/io/HeaderAccessFile.java
@@ -4,6 +4,11 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
+/**
+ * I/O handler for {@code .hdr} files.
+ * 
+ * @author Sung Ho Yoon
+ */
 public final class HeaderAccessFile implements Closeable {
 
     /** The fixed amount of space needed to hold the main variables in the {@code .hdr} file, which apply to all automaton types. */
@@ -11,15 +16,25 @@ public final class HeaderAccessFile implements Closeable {
 
     private String headerFileName;
     private File headerFile;
-    private RandomAccessFile headerRAFile; // Contains basic information about automaton, needed in order to read the bodyFile, as well as the events
+    /** Contains basic information about automaton, needed in order to read the bodyFile, as well as the events */
+    private RandomAccessFile headerRAFile;
 
-
+    /**
+     * Constructs a new {@code HeaderAccessFile} for a given file
+     * @param headerFile a header file
+     * @throws NullPointerException if argument is {@code null}
+     * @throws FileNotFoundException if the given file object cannot be opened
+     */
     public HeaderAccessFile(File headerFile) throws FileNotFoundException {
         this.headerFile = Objects.requireNonNull(headerFile);
         this.headerFileName = this.headerFile.getAbsolutePath();
         headerRAFile = new RandomAccessFile(headerFile, "rw");
     }
 
+    /**
+     * Clears the content of the header file
+     * @return {@code true} the content is cleared; {@code false} otherwise.
+     */
     public boolean clearFile() {
         try {
             headerRAFile.close();
@@ -33,6 +48,15 @@ public final class HeaderAccessFile implements Closeable {
         }
     }
 
+    /**
+     * Reads specified number of bytes from the header, and stores
+     * them in an array.
+     * 
+     * @param length length of the returned array
+     * @return a {@code byte} array containing the bytes read from the header
+     * @throws IllegalArgumentException if argument is negative
+     * @throws IOException if I/O error occurs
+     */
     public byte[] readHeaderBytes(int length) throws IOException {
         if (length < 0) {
             throw new IllegalArgumentException(
@@ -46,39 +70,89 @@ public final class HeaderAccessFile implements Closeable {
         return buffer;
     }
 
+    /**
+     * Closes this header access file and releases any system resource
+     * associated with this.
+     * 
+     * @throws IOException if I/O error occurs
+     */
     @Override
     public void close() throws IOException {
         headerRAFile.close();
     }
 
+    /**
+     * Determines whether the underlying header file exists.
+     * 
+     * @return {@code true} if the underlying header file exists
+     */
     public boolean exists() {
         return headerFile.exists();
     }
 
+    /**
+     * Determines whether the underlying header file is empty
+     * 
+     * @return {@code true} if the underlying header file is empty
+     * @throws IOException if I/O error occurs
+     */
     public boolean isEmpty() throws IOException {
         return headerRAFile.length() == 0;
     }
 
+    /**
+     * Copies the content of the underlying header file to a new file
+     * 
+     * @param newHeaderFile the target destination
+     * @throws IOException if I/O error occurs
+     */
     public void copyTo(File newHeaderFile) throws IOException {
         Files.copy(headerFile.toPath(), newHeaderFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
+    /**
+     * Sets the file pointer offset
+     * 
+     * @param pos the new file pointer offset
+     * @throws IOException if I/O error occurs
+     * 
+     * @see java.io.RandomAccessFile#seek(long)
+     */
     public void seek(long pos) throws IOException {
         headerRAFile.seek(pos);
     }
 
+    /**
+     * Writes the specified bytes to the header file
+     * @param data bytes to write to the header
+     * @throws IOException if I/O error occurs
+     * 
+     * @see java.io.RandomAccessFile#write(byte[])
+     */
     public void write(byte[] data) throws IOException {
-        headerRAFile.write(data);
+        headerRAFile.write(Objects.requireNonNull(data));
     }
 
+    /**
+     * Trim the underlying {@code .hdr} file so that there is no garbage at the end 
+     * @throws IOException if I/O error occurs
+     */
     public void trim() throws IOException {
         headerRAFile.setLength(headerRAFile.getFilePointer());
     }
 
+    /**
+     * Returns the underlying file object
+     * @return the underlying file object
+     */
     public File getHeaderFile() {
         return this.headerFile;
     }
 
+    /**
+     * Returns the string representation of this header access file
+     * @return the string representation of this header access file
+     */
     @Override
     public String toString() {
         return this.headerFileName;
