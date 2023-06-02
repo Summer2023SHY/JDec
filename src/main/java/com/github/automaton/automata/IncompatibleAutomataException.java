a461e254c324e47d9589ac1ecda4924348ae4b91
syoon2 <syoon2@mta.ca>
2023-06-02 16:26:20 +0900

    Add license header to (almost) everything

--- src/main/java/com/github/automaton/automata/IncompatibleAutomataException.java
+++ src/main/java/com/github/automaton/automata/IncompatibleAutomataException.java
@@ -1,4 +1,28 @@
 package com.github.automaton.automata;
+
+/* 
+ * Copyright (C) 2016 Micah Stairs
+ * Copyright (C) 2023 Sung Ho Yoon
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * Thrown when two automata are being combined in some way and they are not
  * compatible with one another. For example, this can happen if they have a
