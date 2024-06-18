d1c59ed40494baa82f48c8fff42fae25b21d1293
syoon2 <syoon2@mta.ca>
2024-06-18 17:15:17 +0900

    Use text blocks in tests

--- jdec-api/src/test/java/com/github/automaton/automata/ObservabilityTest.java
+++ jdec-api/src/test/java/com/github/automaton/automata/ObservabilityTest.java
@@ -51,98 +51,107 @@ public class ObservabilityTest {
         return new Automaton[] {
                 AutomatonGenerator.generateFromGUICode(
                         new Automaton(2),
-                        "alpha,TF,TF\n" + //
-                                "beta,FT,FT\n" + //
-                                "gamma,FF,TT", // Events
-                        "@0,F\n" + //
-                                "1,F\n" + //
-                                "2,F\n" + //
-                                "3,F\n" + //
-                                "4,F\n" + //
-                                "5,F\n" + //
-                                "6,F\n" + //
-                                "7,F\n" + //
-                                "8,F", // States
-                        "0,beta,1\n" + //
-                                "0,alpha,2\n" + //
-                                "1,gamma,3\n" + //
-                                "1,beta,4:BAD\n" + //
-                                "2,gamma,5:BAD\n" + //
-                                "2,beta,6\n" + //
-                                "4,gamma,7:BAD\n" + //
-                                "6,gamma,8:BAD" // Transitions
+                        """
+                                alpha,TF,TF
+                                beta,FT,FT
+                                gamma,FF,TT""", // Events
+                        """
+                                @0,F
+                                1,F
+                                2,F
+                                3,F
+                                4,F
+                                5,F
+                                6,F
+                                7,F
+                                8,F""", // States
+                        """
+                                0,beta,1
+                                0,alpha,2
+                                1,gamma,3
+                                1,beta,4:BAD
+                                2,gamma,5:BAD
+                                2,beta,6
+                                4,gamma,7:BAD
+                                6,gamma,8:BAD""" // Transitions
                 ),
                 AutomatonGenerator.generateFromGUICode(
                         new Automaton(2),
-                        "alpha,TF,FF\n" + //
-                                "beta,FT,FF\n" + //
-                                "gamma,FF,TT\n" + //
-                                "delta,TF,FF\n" + //
-                                "mu,TT,FF", // Events
-                        "@0,F\n" + //
-                                "1,F\n" + //
-                                "2,F\n" + //
-                                "3,F\n" + //
-                                "4,F\n" + //
-                                "5,F\n" + //
-                                "6,F\n" + //
-                                "7,F\n" + //
-                                "8,F", // States
-                        "0,gamma,1\n" + //
-                                "0,mu,2\n" + //
-                                "0,beta,3\n" + //
-                                "0,alpha,4\n" + //
-                                "2,gamma,5\n" + //
-                                "3,beta,2\n" + //
-                                "3,gamma,7:BAD\n" + //
-                                "3,delta,6\n" + //
-                                "4,gamma,7:BAD\n" + //
-                                "6,gamma,8" // Transitions
+                        """
+                                alpha,TF,FF
+                                beta,FT,FF
+                                gamma,FF,TT
+                                delta,TF,FF
+                                mu,TT,FF""", // Events
+                        """
+                                @0,F
+                                1,F
+                                2,F
+                                3,F
+                                4,F
+                                5,F
+                                6,F
+                                7,F
+                                8,F""", // States
+                        """
+                                0,gamma,1
+                                0,mu,2
+                                0,beta,3
+                                0,alpha,4
+                                2,gamma,5
+                                3,beta,2
+                                3,gamma,7:BAD
+                                3,delta,6
+                                4,gamma,7:BAD
+                                6,gamma,8""" // Transitions
                 ),
                 AutomatonGenerator.generateFromGUICode(
                         new Automaton(2),
-                        "a1,TF,FF\n" + //
-                                "a2,TF,FF\n" + //
-                                "a3,TF,FF\n" + //
-                                "a4,TF,FF\n" + //
-                                "b1,FT,FF\n" + //
-                                "b2,FT,FF\n" + //
-                                "b3,FT,FF\n" + //
-                                "b4,FT,FF\n" + //
-                                "sigma,FF,TT", // Events
-                        "@0,F\n" + //
-                                "1,F\n" + //
-                                "2,F\n" + //
-                                "3,F\n" + //
-                                "4,F\n" + //
-                                "5,F\n" + //
-                                "6,F\n" + //
-                                "7,F\n" + //
-                                "8,F\n" + //
-                                "9,F\n" + //
-                                "10,F\n" + //
-                                "11,F\n" + //
-                                "12,F", // States
-                        "0,a1,1\n" + //
-                                "0,a2,2\n" + //
-                                "0,a3,3\n" + //
-                                "0,a4,4\n" + //
-                                "1,b1,5\n" + //
-                                "1,b2,6\n" + //
-                                "2,b2,7\n" + //
-                                "2,b3,8\n" + //
-                                "3,b3,9\n" + //
-                                "3,b4,10\n" + //
-                                "4,b4,11\n" + //
-                                "4,b1,12\n" + //
-                                "5,sigma,5\n" + //
-                                "6,sigma,6:BAD\n" + //
-                                "7,sigma,7\n" + //
-                                "8,sigma,8:BAD\n" + //
-                                "9,sigma,9\n" + //
-                                "10,sigma,10:BAD\n" + //
-                                "11,sigma,11:BAD\n" + //
-                                "12,sigma,12" // Transitions
+                        """
+                                a1,TF,FF
+                                a2,TF,FF
+                                a3,TF,FF
+                                a4,TF,FF
+                                b1,FT,FF
+                                b2,FT,FF
+                                b3,FT,FF
+                                b4,FT,FF
+                                sigma,FF,TT""", // Events
+                        """
+                                @0,F
+                                1,F
+                                2,F
+                                3,F
+                                4,F
+                                5,F
+                                6,F
+                                7,F
+                                8,F
+                                9,F
+                                10,F
+                                11,F
+                                12,F""", // States
+                        """
+                                0,a1,1
+                                0,a2,2
+                                0,a3,3
+                                0,a4,4
+                                1,b1,5
+                                1,b2,6
+                                2,b2,7
+                                2,b3,8
+                                3,b3,9
+                                3,b4,10
+                                4,b4,11
+                                4,b1,12
+                                5,sigma,5
+                                6,sigma,6:BAD
+                                7,sigma,7
+                                8,sigma,8:BAD
+                                9,sigma,9
+                                10,sigma,10:BAD
+                                11,sigma,11:BAD
+                                12,sigma,12""" // Transitions
                 ),
                 fig6Adapter.getAutomaton()
         };
@@ -161,30 +170,33 @@ public class ObservabilityTest {
         return new Automaton[] {
                 AutomatonGenerator.generateFromGUICode(
                         new Automaton(2),
-                        "a1,TF,FF\n" +
-                                "a2,TF,FF\n" +
-                                "b1,FT,FF\n" +
-                                "b2,FT,FF\n" +
-                                "sigma,FF,TT", // Events
+                        """
+                                a1,TF,FF
+                                a2,TF,FF
+                                b1,FT,FF
+                                b2,FT,FF
+                                sigma,FF,TT""", // Events
 
-                        "@1,F\n" +
-                                "2,F\n" +
-                                "3,F\n" +
-                                "4,F\n" +
-                                "5,F\n" +
-                                "6,F\n" +
-                                "7,F", // States
+                        """
+                                @1,F
+                                2,F
+                                3,F
+                                4,F
+                                5,F
+                                6,F
+                                7,F""", // States
 
-                        "1,a1,2\n" +
-                                "1,a2,3\n" +
-                                "2,b1,4\n" +
-                                "2,b2,5\n" +
-                                "3,b1,6\n" +
-                                "3,b2,7\n" +
-                                "4,sigma,4\n" +
-                                "5,sigma,5:BAD\n" +
-                                "6,sigma,6:BAD\n" +
-                                "7,sigma,7" // Transitions
+                        """
+                                1,a1,2
+                                1,a2,3
+                                2,b1,4
+                                2,b2,5
+                                3,b1,6
+                                3,b2,7
+                                4,sigma,4
+                                5,sigma,5:BAD
+                                6,sigma,6:BAD
+                                7,sigma,7""" // Transitions
                 ),
                 fig1Adapter.getAutomaton()
         };
