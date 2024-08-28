c67134c6b1284525e6b6e88d0b0834ee40412252
syoon2 <syoon2@mta.ca>
2024-08-28 11:53:56 +0900

    Add Javadoc

--- jdec-api/src/main/java/com/github/automaton/automata/incremental/PlantOverSpecComponentIterable.java
+++ jdec-api/src/main/java/com/github/automaton/automata/incremental/PlantOverSpecComponentIterable.java
@@ -9,6 +9,12 @@ import java.util.*;
 
 import com.github.automaton.automata.Automaton;
 
+/**
+ * An iterable of system components that favor plants over specifications.
+ * 
+ * @author Sung Ho Yoon
+ * @since 2.1.0
+ */
 public class PlantOverSpecComponentIterable extends HeuristicBasedComponentIterable {
 
     Comparator<Automaton> ordering = (aut1, aut2) -> {
@@ -24,11 +30,29 @@ public class PlantOverSpecComponentIterable extends HeuristicBasedComponentItera
         }
     };
 
+    /**
+     * Constructs a new {@code PlantOverSpecComponentIterable}.
+     * 
+     * @param plants the set of plants
+     * @param specs the set of specifications
+     * 
+     * @throws NullPointerException if either one of the arguments is {@code null}
+     */
     public PlantOverSpecComponentIterable(Set<Automaton> plants, Set<Automaton> specs) {
         super(plants, specs);
         setOrdering(ordering);
     }
 
+    /**
+     * Constructs a new {@code PlantOverSpecComponentIterable}.
+     * 
+     * @param plants the set of plants
+     * @param specs the set of specifications
+     * @param gPrime the set of "checked" plants
+     * @param hPrime the set of "checked" specifications
+     * 
+     * @throws NullPointerException if any one of the arguments is {@code null}
+     */
     public PlantOverSpecComponentIterable(Set<Automaton> plants, Set<Automaton> specs, Set<Automaton> gPrime, Set<Automaton> hPrime) {
         this(plants, specs);
         setFilters(gPrime, hPrime);
