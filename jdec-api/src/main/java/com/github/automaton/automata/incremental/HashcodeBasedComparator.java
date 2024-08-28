34e9893dab889f6f362f488c2fcba18beb3a1e93
syoon2 <syoon2@mta.ca>
2024-08-28 11:53:56 +0900

    Add Javadoc

--- jdec-api/src/main/java/com/github/automaton/automata/incremental/HashcodeBasedComparator.java
+++ jdec-api/src/main/java/com/github/automaton/automata/incremental/HashcodeBasedComparator.java
@@ -22,13 +22,23 @@ public final class HashcodeBasedComparator<T> implements Comparator<T> {
     private HashcodeBasedComparator() {
     }
 
+    /**
+     * Returns the sole instance of {@code HashcodeBasedComparator}.
+     * 
+     * @param <T> the data type
+     * @return the sole instance
+     */
     @SuppressWarnings("unchecked")
     public static <T> HashcodeBasedComparator<T> instance() {
         return (HashcodeBasedComparator<T>) INSTANCE;
     }
 
     /**
-     * Compares and returns the two objects' orders. 
+     * Compares and returns the two objects' orders.
+     * 
+     * @return a negative integer, zero, or a positive integer as the
+     *         first argument is less than, equal to, or greater than the
+     *         second
      */
     @Override
     public int compare(T o1, T o2) {
--- jdec-api/src/main/java/com/github/automaton/automata/incremental/RandomOrderComponentIterable.java
+++ jdec-api/src/main/java/com/github/automaton/automata/incremental/RandomOrderComponentIterable.java
@@ -9,13 +9,39 @@ import java.util.*;
 
 import com.github.automaton.automata.Automaton;
 
+/**
+ * An iterable of system components that uses random ordering.
+ * Specifically, the {@link Object#hashCode() hash codes} of
+ * the components are used for ordering.
+ * 
+ * @author Sung Ho Yoon
+ * @since 2.1.0
+ */
 public class RandomOrderComponentIterable  extends HeuristicBasedComponentIterable {
 
+    /**
+     * Constructs a new {@code RandomOrderComponentIterable}.
+     * 
+     * @param plants the set of plants
+     * @param specs the set of specifications
+     * 
+     * @throws NullPointerException if either one of the arguments is {@code null}
+     */
     public RandomOrderComponentIterable(Set<Automaton> plants, Set<Automaton> specs) {
         super(plants, specs);
         setOrdering(HashcodeBasedComparator.instance());
     }
 
+    /**
+     * Constructs a new {@code RandomOrderComponentIterable}.
+     * 
+     * @param plants the set of plants
+     * @param specs the set of specifications
+     * @param gPrime the set of "checked" plants
+     * @param hPrime the set of "checked" specifications
+     * 
+     * @throws NullPointerException if any one of the arguments is {@code null}
+     */
     public RandomOrderComponentIterable(Set<Automaton> plants, Set<Automaton> specs, Set<Automaton> gPrime, Set<Automaton> hPrime) {
         this(plants, specs);
         setFilters(gPrime, hPrime);
