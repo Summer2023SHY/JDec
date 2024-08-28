fb64fa5d0d84e99d466396651ff06e8a07cbdec6
syoon2 <syoon2@mta.ca>
2024-08-28 13:28:26 +0900

    Allow use of heuristics in incremental tests

---
+++ jdec-api/src/main/java/com/github/automaton/automata/incremental/FilteredComponentIterableGenerator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) Sung Ho Yoon. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ */
+
+package com.github.automaton.automata.incremental;
+
+import java.util.Collections;
+import java.util.Set;
+
+import com.github.automaton.automata.Automaton;
+
+/**
+ * A generator interface for {@link HeuristicBasedComponentIterable}s,
+ * with an option to specify extra filters.
+ * 
+ * @author Sung Ho Yoon
+ * @since 2.1.0
+ */
+@FunctionalInterface
+public interface FilteredComponentIterableGenerator extends ComponentIterableGenerator {
+
+    @Override
+    public default HeuristicBasedComponentIterable generate(Set<Automaton> plants, Set<Automaton> specs) {
+        return generate(plants, specs, Collections.emptySet(), Collections.emptySet());
+    }
+
+    public HeuristicBasedComponentIterable generate(Set<Automaton> plants, Set<Automaton> specs, Set<Automaton> gPrime, Set<Automaton> hPrime);
+
+}
