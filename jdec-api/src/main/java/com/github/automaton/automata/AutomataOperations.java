1a3b70c33976ef236748c9ca85cf6efcccc72418
syoon2 <syoon2@mta.ca>
2024-09-04 15:47:23 +0900

    Incremental: Correctly mark transitions as "bad"

--- jdec-api/src/main/java/com/github/automaton/automata/AutomataOperations.java
+++ jdec-api/src/main/java/com/github/automaton/automata/AutomataOperations.java
@@ -1129,6 +1129,7 @@ public class AutomataOperations {
         Automaton compositePlant = buildCompositeAutomaton(plants);
         Automaton compositeSpec = buildCompositeAutomaton(specs);
         Automaton combinedSys = intersection(compositePlant.generateTwinPlant(), compositeSpec.generateTwinPlant());
+        BitSet bSet = new BitSet();
         for (long stateId = 1; stateId <= combinedSys.nStates; stateId++) {
             if (!combinedSys.stateExists(stateId))
                 continue;
@@ -1143,7 +1144,12 @@ public class AutomataOperations {
                 combinedSys.removeState(stateId);
             } else if (dumpCount > 0) {
                 s.setMarked(true);
+                bSet.set((int) stateId);
             }
+        }
+        var bad = combinedSys.getAllTransitions().parallelStream().filter(td -> bSet.get((int) td.targetStateID)).toList();
+        for (var badTd : bad) {
+            combinedSys.markTransitionAsBad(badTd.initialStateID, badTd.eventID, badTd.targetStateID);
         }
         combinedSys.renumberStates();
         return combinedSys;
