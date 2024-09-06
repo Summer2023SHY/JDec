31aba2aac526a5a541771f7d2c83b7c3f8dd8e71
syoon2 <syoon2@mta.ca>
2024-09-06 11:46:21 +0900

    Add missing Javadoc

---
+++ jdec-api/src/main/java/com/github/automaton/automata/incremental/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) Sung Ho Yoon. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ */
+
+/**
+ * Provides options and utilities for
+ * {@link com.github.automaton.automata.AutomataOperations#testIncrementalObservability(java.util.Set, java.util.Set, CounterexampleHeuristics, FilteredComponentIterableGenerator)
+ * testing incremental observability}.
+ * 
+ * @author Sung Ho Yoon
+ * @since 2.1.0
+ */
+package com.github.automaton.automata.incremental;
